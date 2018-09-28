198fea5aa94be6b77c5be3b69758e9c228e3fc65
BlindMoron <43655591+BlindMoron@users.noreply.github.com>
2018-09-28 21:07:28 +0500

    123

--- tests/difficulty_test_util.go
+++ tests/difficulty_test_util.go
@@ -47,24 +47,3 @@ type difficultyTestMarshaling struct {
 	UncleHash          common.Hash
 	CurrentBlockNumber math.HexOrDecimal64
 }
-
-func (test *DifficultyTest) Run(config *params.ChainConfig) error {
-	parentNumber := big.NewInt(int64(test.CurrentBlockNumber - 1))
-	parent := &types.Header{
-		Difficulty: test.ParentDifficulty,
-		Time:       test.ParentTimestamp,
-		Number:     parentNumber,
-		UncleHash:  test.UncleHash,
-	}
-	var chain = consensus.ChainReader
-	actual := ethash.CalcDifficulty(chain, config, test.CurrentTimestamp.Uint64(), parent)
-	exp := test.CurrentDifficulty
-
-	if actual.Cmp(exp) != 0 {
-		return fmt.Errorf("parent[time %v diff %v unclehash:%x] child[time %v number %v] diff %v != expected %v",
-			test.ParentTimestamp, test.ParentDifficulty, test.UncleHash,
-			test.CurrentTimestamp, test.CurrentBlockNumber, actual, exp)
-	}
-	return nil
-
-}
