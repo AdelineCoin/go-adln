8f3139fc95c5a4b266764fc8f56588e73bfb4d37
BlindMoron <43655591+BlindMoron@users.noreply.github.com>
2018-09-28 21:08:56 +0500

    123

--- tests/difficulty_test_util.go
+++ tests/difficulty_test_util.go
@@ -17,15 +17,10 @@
 package tests
 
 import (
-	"fmt"
 	"math/big"
 
 	"github.com/AdelineCoin/go-adln/common"
 	"github.com/AdelineCoin/go-adln/common/math"
-	"github.com/AdelineCoin/go-adln/consensus/ethash"
-	"github.com/AdelineCoin/go-adln/consensus"
-	"github.com/AdelineCoin/go-adln/core/types"
-	"github.com/AdelineCoin/go-adln/params"
 )
 
 //go:generate gencodec -type DifficultyTest -field-override difficultyTestMarshaling -out gen_difficultytest.go
